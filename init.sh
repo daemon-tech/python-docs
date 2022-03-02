da74859fb0f00b747cb3acb284932e0de416486e
le3ch-tech <sam.moes15@gmail.com>
2022-03-02 01:27:14 +0100

    initial submit

---
+++ init.sh
@@ -0,0 +1,7 @@
+echo "# python-docs" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/le3ch-tech/python-docs.git
+git push -u origin main
